fa17b285342965cad3f93f259f2578e0648d4b07
Tyler Stewart <tylerjosephstewart1@gmail.com>
2021-01-21 01:32:49 -0800

    made custom directive to display stars rating

--- App/public/angular-app/hotel-data-factory/hotel-data-factory.js
+++ App/public/angular-app/hotel-data-factory/hotel-data-factory.js
@@ -4,9 +4,9 @@ function hotelDataFactory($http){
     return{
         hotelList : hotelList,
         hotelDisplay : hotelDisplay
-    };
+    }
     function hotelList() {
-        return $http.get('/hotels?count=12').then(complete).catch(failed);
+        return $http.get('/hotels?count=10').then(complete).catch(failed);
     }
     function hotelDisplay(id){
         return $http.get('/hotels/' + id).then(complete).catch(failed);
